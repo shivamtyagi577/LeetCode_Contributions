f34bae9f996484f1ed1380d01d48dcacad73c0e4
Shivam Tyagi <39437296+shivamtyagi577@users.noreply.github.com>
2025-09-02 12:33:33 +0530

    Time: 146 ms (33.33%) | Memory: 18 MB (20.51%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
@@ -0,0 +1,28 @@
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        n = len(points)
+        count = 0
+
+        for i in range(n):
+            for j in range(n):
+                if i == j:
+                    continue
+
+                x1, y1 = points[i]
+                x2, y2 = points[j]
+                # Check if point i on the upper left side of point j
+                if x1 <= x2 and y1 >= y2:
+                    valid = True
+
+                    for k in range(n):
+                        if k != i and k!= j:
+                            x, y  = points[k]
+
+                            # Check if point k lies inside or on the border of the rectangle
+                            if x1 <= x <= x2 and y2 <= y <= y1:
+                                valid = False
+                                break
+                    
+                    if valid:
+                        count += 1
+        return count
