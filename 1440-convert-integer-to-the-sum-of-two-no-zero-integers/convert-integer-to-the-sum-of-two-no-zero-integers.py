028054bf4d2f0a1d33557bbf5f1f7f679d1e42e7
Shivam Tyagi <39437296+shivamtyagi577@users.noreply.github.com>
2025-09-08 13:41:12 +0530

    Time: 7 ms (23.90%) | Memory: 12.6 MB (61.64%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -1,12 +1,7 @@
 class Solution(object):
     def getNoZeroIntegers(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
         def check(x):
             return '0' not in str(x)
-
         for i in range(1, n):
             j = n - i
             if check(i) and check(j):
