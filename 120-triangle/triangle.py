d5eafcf8f7a127708903fe53c0d4e76f350eae56
Shivam Tyagi <39437296+shivamtyagi577@users.noreply.github.com>
2025-09-25 23:04:18 +0530

    Time: 3 ms (86.00%) | Memory: 18.8 MB (74.79%) - LeetSync

---
+++ 120-triangle/triangle.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = triangle[-1]
+
+        # Iterate from second last row to the top
+        for row in range(len(triangle) - 2,-1, -1):
+            for i in range(len(triangle[row])):
+                # Update dp[i] to be a min path sum from this element
+                dp[i] = triangle[row][i] + min(dp[i], dp[i+1])
+        
+        return dp[0]
