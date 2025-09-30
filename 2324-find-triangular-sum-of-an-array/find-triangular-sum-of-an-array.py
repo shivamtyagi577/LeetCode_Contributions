a677e148b2522e303c92f7db6878a74afad0d397
Shivam Tyagi <39437296+shivamtyagi577@users.noreply.github.com>
2025-09-30 18:26:50 +0530

    Time: 1197 ms (64.21%) | Memory: 17.9 MB (61.29%) - LeetSync

--- 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.py
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.py
@@ -3,4 +3,4 @@ class Solution:
         while len(nums) > 1:
             nums = [(nums[i] + nums[i+1]) % 10 for i in range(len(nums) - 1)]
         return nums[0]
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
+#__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("000"))
