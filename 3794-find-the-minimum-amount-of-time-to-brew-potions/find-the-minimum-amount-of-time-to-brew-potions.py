d721ad979f4fe87ab0f681a921477cfaec829ee1
Shivam Tyagi <39437296+shivamtyagi577@users.noreply.github.com>
2025-10-10 00:54:47 +0530

    Time: 12137 ms (30.00%) | Memory: 13.5 MB (10.00%) - LeetSync

---
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def minTime(self, skill, mana):
+        n, m = len(skill), len(mana)
+        times = [0] * n
+        for j in range(m):
+            cur_time = 0
+            for i in range(n):
+                cur_time = max(cur_time, times[i]) + skill[i] * mana[j]
+            times[n - 1] = cur_time
+            for i in range(n - 2, -1, -1):
+                times[i] = times[i + 1] - skill[i + 1] * mana[j]
+        return times[n - 1]
