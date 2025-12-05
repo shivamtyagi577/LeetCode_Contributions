1535ea8a3499852af5c3ec79e656fa5815241309
Shivam Tyagi <39437296+shivamtyagi577@users.noreply.github.com>
2025-12-05 15:59:29 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.20%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
@@ -1,9 +1,8 @@
 class Solution:
     def countPartitions(self, nums: List[int]) -> int:        
         total_sum = sum(nums)
-        n = len(nums)
         
         if total_sum % 2 == 0:
-            return n - 1  # all partitions valid
+            return len(nums) - 1  # all partitions valid
     
         return 0
